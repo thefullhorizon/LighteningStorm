186bca519449073f267fb130cc7f5078b1d8a749
zhaoyang <zhaoyang.sh@superjia.com>
2017-09-12 11:38:14 +0800

    Initial commit

---
+++ app/src/main/java/com/nanshan/lighteningstorm/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nanshan.lighteningstorm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
